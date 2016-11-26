29b09c9c5ae65a4cb816ce9ff6d77d2bf548f695
Pouyan <pouyank13@me.com>
2016-11-25 17:09:54 -0700

    Okay now loops are correct with correct indexs I THINK as well. Likilhood still looks okay error is still wrong.

--- RLSL_algorithm.m
+++ RLSL_algorithm.m
@@ -23,26 +23,27 @@ e = zeros(M+1,N+1);
 for n = 2:N %is that right?
     % should these be n-1? 
     %u_vec = u(n:-1:n-M+1);
-    b(1,n) = u(n);
-    f(1,n) = u(n);
-    F(1,n) = lamda*F(1,n-1)+u(n)^2;
+    b(2,n) = u(n);
+    f(2,n) = u(n);
+    F(2,n) = lamda*F(2,n-1)+u(n)^2;
     e(1,n)= d(n);
-    gamma_s(1,n) = 1;
-    B(1,n) = F(1,n);
+    gamma_s(2,n) = 1;
+    B(2,n) = F(2,n);
     for m = 2:M+1
-        Delta(m-1,n) = Delta(m-1,n-1) + b(m-1,n-1)*f(m-1,n)/(gamma_s(m-1,n-1));
-        gamma_f(m,n) = -Delta(m-1,n)/B(m-1,n-1);
-        gamma_b(m,n) = -Delta(m-1,n)/F(m-1,n);
-        rho(m,n) = lamda*rho(m,n-1)+ b(m,n)/gamma_s(m,n)*e(m-1,n);
-        kap(m,n) = rho(m,n)/B(m,n);
+        Delta(m,n) = Delta(m,n-1) + b(m,n-1)*f(m,n)/(gamma_s(m,n-1));
+        gamma_f(m+1,n) = -Delta(m,n)/B(m,n-1);
+        gamma_b(m+1,n) = -Delta(m,n)/F(m,n);
+        rho(m-1,n) = lamda*rho(m-1,n-1)+ b(m,n)/gamma_s(m,n)*e(m-1,n); %might need work
+        kap(m-1,n) = rho(m-1,n)/B(m,n);%might need work
         
-        f(m,n) = f(m-1,n) + gamma_f(m,n)*b(m-1,n-1);
-        b(m,n) = b(m-1,n-1)+ gamma_b(m,n)*f(m-1,n);
-        F(m,n) = F(m-1,n) + gamma_f(m,n)*Delta(m-1,n);
-        B(m,n)= B(m-1,n-1)+ gamma_b(m,n)*Delta(m-1,n);
-        gamma_s(m,n) = gamma_s(m-1,n) - b(m-1,n)^2/B(m-1,n);
-        % don't forget to adjust for delay
-        e(m,n) = e(m-1,n) - kap(m-1,n)*b(m-1,n);
+        f(m+1,n) = f(m,n) + gamma_f(m+1,n)*b(m,n-1);
+        b(m+1,n) = b(m,n-1)+ gamma_b(m+1,n)*f(m,n);
+        F(m+1,n) = F(m,n) + gamma_f(m+1,n)*Delta(m,n);
+        B(m+1,n)= B(m,n-1)+ gamma_b(m+1,n)*Delta(m,n);
+        gamma_s(m+1,n) = gamma_s(m,n) - b(m,n)^2/B(m,n);
+        % NOTE IN ALL THE ESTIMATION CALCS I HAVE LEFT THE PREDICTION
+        % VARIABLE m AS IS FOR PREDICTION
+        e(m,n) = e(m-1,n) - kap(m-1,n)*b(m,n);
     end
 end
 
