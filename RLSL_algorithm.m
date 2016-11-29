2bf4cac08d143b24efc584f1f18d30240c1763b4
Pouyan <pouyank13@me.com>
2016-11-29 14:26:25 -0700

    Moved estimator to different loop. Now getting consistent results

--- RLSL_algorithm.m
+++ RLSL_algorithm.m
@@ -10,42 +10,43 @@ function [e,gamma_s] = RLSL_algorithm(M,N,lamda,delta,d,u)
     % W: Tap weight vector
     % e: estimation error 
 %% Initialization
-b = zeros(M+1,N+1);
-f = zeros(M+1,N+1);
-B = delta.*ones(M+1,N+1);
-F = delta.*ones(M+1,N+1);
-Delta = zeros(M+1,N+1);
-gamma_s = ones(M+1,N+1);
-rho = zeros(M+1,N+1);
-e = zeros(M+1,N+1);
-%u = u(:);
+b = zeros(M+1,N);
+f = zeros(M+1,N);
+B = delta.*ones(M+1,N);
+F = delta.*ones(M+1,N);
+Delta = zeros(M+1,N);
+gamma_s = ones(M+1,N);
+rho = zeros(M+1,N);
 
-for n = 2:N %is that right?
-    % should these be n-1? 
-    %u_vec = u(n:-1:n-M+1);
+for n = 2:N 
+
     b(1,n) = u(n);
     f(1,n) = u(n);
     F(1,n) = lamda*F(1,n-1)+u(n)^2;
-    e(1,n)= d(n);
     gamma_s(1,n) = 1;
     B(1,n) = F(1,n);
-    for m = 1:M+1
-        Delta(m,n) = Delta(m,n-1) + b(m,n-1)*f(m,n)/(gamma_s(m,n-1));
-        gamma_f(m+1,n) = -Delta(m,n)/B(m,n-1);
-        gamma_b(m+1,n) = -Delta(m,n)/F(m,n);
-        
-        rho(m,n) = lamda*rho(m,n-1)+ b(m,n)/gamma_s(m,n)*e(m,n); %might need work
-        kap(m,n) = rho(m,n)/B(m,n);%might need work
-        
-        f(m+1,n) = f(m,n) + gamma_f(m+1,n)*b(m,n-1);
-        b(m+1,n) = b(m,n-1)+ gamma_b(m+1,n)*f(m,n);
-        F(m+1,n) = F(m,n) + gamma_f(m+1,n)*Delta(m,n);
-        B(m+1,n)= B(m,n-1)+ gamma_b(m+1,n)*Delta(m,n);
-        gamma_s(m+1,n) = gamma_s(m,n) - b(m,n)^2/B(m,n);
-        
-        e(m+1,n) = e(m,n) - kap(m,n)*b(m,n);
-        
+    for m = 2:M+1
+        Delta(m-1,n) = Delta(m-1,n-1) + b(m-1,n-1)*f(m-1,n)/(gamma_s(m-1,n-1));
+        gamma_f(m,n) = -Delta(m-1,n)/B(m-1,n-1);
+        gamma_b(m,n) = -Delta(m-1,n)/F(m-1,n);
+             
+        f(m,n) = f(m-1,n) + gamma_f(m,n)*b(m-1,n-1);
+        b(m,n) = b(m-1,n-1)+ gamma_b(m,n)*f(m-1,n);
+        F(m,n) = F(m-1,n) + gamma_f(m,n)*Delta(m-1,n);
+        B(m,n)= B(m-1,n-1)+ gamma_b(m,n)*Delta(m-1,n);
+        gamma_s(m,n) = gamma_s(m-1,n) - b(m-1,n)^2/B(m-1,n);
+            
     end
+end
+
+for n = 2:N
+    e(1,n)= d(n);
+    for m = 1:M
+        rho(m,n) = lamda*rho(m,n-1)+ b(m,n)/gamma_s(m,n)*e(m,n);
+        kap(m,n) = rho(m,n)/B(m,n);
+        e(m+1,n) = e(m,n) - kap(m,n)*b(m,n);
+    end
+
 end
 
 end
--- enel671_project3.m
+++ enel671_project3.m
@@ -16,14 +16,14 @@ for k=1:K
       a = BPSK(N);
       u = filterinput(a,h); 
       [e, gamma_s] = RLSL_algorithm(M,N,lamda,delta,a,u(:,1));
-      e12 = e(12,:);
-      e12 = e12(:);
-      ed12(:,k) = e12.^2;
-      MSEE11 = sum(ed12,2)/K;
-      MSEE11n = MSEE11./gamma_s(12,:)';
+      e11 = e(11,:);
+      e11 = e11(:);
+      ed11(:,k) = e11.^2;
+      MSEE11 = sum(ed11,2)/K;
+      MSEE11n = MSEE11./gamma_s(11,:)';
 end
     figure(1)
-    plot(1:N+1,gamma_s(11,:),'LineWidth',2)
+    plot(1:N,gamma_s(11,:),'LineWidth',2)
     legend('Channel 1','Channel 2','Channel 3','Channel 4')
     grid on
     xlabel('Time (s)');
@@ -31,4 +31,5 @@ end
     title('Likilihood');
 
     figure(2)
-    semilogy(1:N+1,MSEE11n,'LineWidth',2)
+    semilogy(1:N,MSEE11n,'LineWidth',2)
+    title('Normalized MSEE');
