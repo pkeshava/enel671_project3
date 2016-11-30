61c58c4ccebb8b8bf50c11e8129aeefe7373ecea
Pouyan <pouyank13@me.com>
2016-11-30 15:04:49 -0700

    bs

--- RLSL_algorithm.m
+++ RLSL_algorithm.m
@@ -18,34 +18,36 @@ gamma_s = ones(M+1,N);
 rho = zeros(M+1,N);
 b(1,1) = u(1);
 f(1,1) = u(1);
-
-for n = 2:N 
-    
+no = 1;
+for n = 1:N 
+    if n > 1
+        no = n-1;
+    end
     b(1,n) = u(n);
     f(1,n) = u(n);
     gamma_s(1,n) = 1;
-    F(1,n) = lamda*F(1,n-1)+u(n)^2;
+    F(1,n) = lamda*F(1,no)+u(n)^2;
     B(1,n) = F(1,n);
     for m = 2:M+1
         
-            Delta(m-1,n) = Delta(m-1,n-1) + b(m-1,n-1)*f(m-1,n)/(gamma_s(m-1,n-1));
-            gamma_f(m,n) = -Delta(m-1,n)/B(m-1,n-1);  
+            Delta(m-1,n) = Delta(m-1,no) + b(m-1,no)*f(m-1,n)/(gamma_s(m-1,no));
+            gamma_f(m,n) = -Delta(m-1,n)/B(m-1,no);  
             gamma_b(m,n) = -Delta(m-1,n)/F(m-1,n);
 
-            f(m,n) = f(m-1,n) + gamma_f(m,n)*b(m-1,n-1);
-            b(m,n) = b(m-1,n-1)+ gamma_b(m,n)*f(m-1,n);
+            f(m,n) = f(m-1,n) + gamma_f(m,n)*b(m-1,no);
+            b(m,n) = b(m-1,no)+ gamma_b(m,n)*f(m-1,n);
             F(m,n) = F(m-1,n) + gamma_f(m,n)*Delta(m-1,n);
-            B(m,n)= B(m-1,n-1)+ gamma_b(m,n)*Delta(m-1,n);
+            B(m,n)= B(m-1,no)+ gamma_b(m,n)*Delta(m-1,n);
             gamma_s(m,n) = gamma_s(m-1,n) - b(m-1,n)^2/B(m-1,n);
             
     end
 end
 
-for n = 2:N
+for n = 1:N
     e(1,n)= d(n);
     for m = 1:M
         %rho(m,1) = 0;
-        rho(m,n) = lamda*rho(m,n-1)+ b(m,n)*e(m,n)/gamma_s(m,n);
+        rho(m,n) = lamda*rho(m,no)+ b(m,n)*e(m,n)/gamma_s(m,n);
         kap(m,n) = rho(m,n)/B(m,n);
         e(m+1,n) = e(m,n) - kap(m,n)*b(m,n);
     end
